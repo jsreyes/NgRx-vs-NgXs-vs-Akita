509c45c860b86149f21b3f6e1bf6204929d4cbf8
Freddy Hernán Martínez Quiñones <fmartinez@belatrix.com>
2019-07-29 09:22:28 -0500

    define action names as constants

--- src/app/store/name.actions.ts
+++ src/app/store/name.actions.ts
@@ -1,11 +1,14 @@
+const SET_NAME = '[Name] Set';
+const RESET_NAME = '[Name] Reset';
+
 export class SetName {
-    static readonly type = '[Name] Set';
+    static readonly type = SET_NAME;
 
     constructor(public payload: string) {}
 }
 
 export class ResetName {
-    static readonly type = '[Name] Reset';
+    static readonly type = RESET_NAME;
 
     constructor() {}
 }
